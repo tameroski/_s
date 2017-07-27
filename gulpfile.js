b2f3b8220e412a5747bb7ba77a2e24a90c6931ab
Tameroski <jgrosdenier@gmail.com>
2017-07-27 17:32:47 +0200

    Introducing Bourbon and Neat

--- gulpfile.js
+++ gulpfile.js
@@ -12,13 +12,20 @@ gulp.task('style_clean', function () {
 gulp.task('style_main', [], function(){
 	return gulp.src('sass/style.scss')
 		.pipe($.sass({
-			outputStyle: 'compressed'
+			includePaths: ["styles"].concat(require('bourbon').includePaths).concat(require('bourbon-neat').includePaths)
 		}).on('error', $.sass.logError))
+		.pipe($.cleanCss({
+			compatibility: 'ie10'
+		}))
 		.pipe(gulp.dest('./'))
 });
 
 gulp.task('style', [ 'style_clean' ], function(){
 	gulp.start('style_main');
+});
+
+gulp.task('init', [], function(){
+	
 });
 
 gulp.task('default', [ 'style' ]);
